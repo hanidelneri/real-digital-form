234d0f09b587b1075d61609fc04eef373154ec24
Hanie Gharedaghi <h7.gharedaghi@gmail.com>
2021-10-23 10:39:43 +0330

    adding form component and tests

--- src/components/form/RealDigitalForm.vue
+++ src/components/form/RealDigitalForm.vue
@@ -1,19 +1,71 @@
 <template>
-  <form novalidate onsubmit="return false">
-    <slot />
-  </form>
+  <div>
+    <p v-if="errorMessage">{{ errorMessage }}</p>
+    <form novalidate onsubmit="return false">
+      <slot />
+    </form>
+  </div>
 </template>
 <script>
 import { REAL_DIGITAL_FORM, REAL_DIGITAL_TEXTFIELD } from "@/components/names";
+import axios from "axios";
 export default {
   name: REAL_DIGITAL_FORM,
+  props: {
+    action: {
+      type: String,
+      required: true,
+    },
+    method: {
+      type: String,
+      default: "POST",
+    },
+  },
+  data() {
+    return {
+      validationStatus: [],
+      errorMessage: "",
+    };
+  },
+  computed: {
+    isValid() {
+      return this.validationStatus.every((status) => status);
+    },
+  },
   methods: {
-    submit() {
-      debugger;
-      const validationStatus = this.$children
-        .filter((child) => child.$options.name === REAL_DIGITAL_TEXTFIELD)
-        .map((child) => child.validate());
-      console.log(validationStatus);
+    async submit() {
+      try {
+        this.setValidationStatus();
+        if (this.isValid) {
+          const values = this.getValues();
+          this.$emit("onSubmit", values);
+          const response = await axios({
+            method: this.method,
+            url: this.action,
+            data: values,
+          });
+          this.resetErrorMessage();
+          this.$emit("onResponse", response.json);
+        }
+      } catch (error) {
+        this.errorMessage = error.message;
+      }
+    },
+    setValidationStatus() {
+      this.validationStatus = this.getInputs().map((child) => child.validate());
+    },
+    getInputs() {
+      return this.$children.filter(
+        (child) => child.$options.name === REAL_DIGITAL_TEXTFIELD
+      );
+    },
+    getValues() {
+      return this.getInputs()
+        .map((input) => ({ [input.name]: input.value }))
+        .reduce((current, acc) => ({ ...acc, ...current }), {});
+    },
+    resetErrorMessage() {
+      this.errorMessage = "";
     },
   },
 };
--- src/components/form/real-digital-form.spec.js
+++ src/components/form/real-digital-form.spec.js
@@ -1,28 +1,87 @@
 import { shallowMount } from "@vue/test-utils";
 import RealDigitalForm from "@/components/form/RealDigitalForm.vue";
 import RealDigitalButton from "@/components/button/RealDigitalButton.vue";
+import axios from "axios";
+
+jest.mock("axios");
+
+function getWrapper(mockValidate) {
+  const wrapper = shallowMount(RealDigitalForm, {
+    slots: {
+      default:
+        "<real-digital-textfield name='test' /> <real-digital-button> submit</real-digital-button/>",
+    },
+    propsData: {
+      action: "",
+    },
+    components: {
+      "real-digital-textfield": {
+        name: "real-digital-textfield",
+        render(createElement) {
+          return createElement("div");
+        },
+        props: {
+          name: {
+            type: String,
+          },
+        },
+        data() {
+          return {
+            value: 2,
+          };
+        },
+        methods: {
+          validate: mockValidate,
+        },
+      },
+      "real-digital-button": RealDigitalButton,
+    },
+  });
+  return wrapper;
+}
+
+async function submit(wrapper) {
+  const button = wrapper.findComponent(RealDigitalButton);
+  await button.trigger("click");
+}
 
 describe("form", () => {
   it("validates inputs on submit", async () => {
     const mockValidate = jest.fn();
-    const wrapper = shallowMount(RealDigitalForm, {
-      slots: {
-        default:
-          "<real-digital-textfield /> <real-digital-button> submit</real-digital-button/>",
-      },
-      components: {
-        "real-digital-textfield": {
-          name: "real-digital-textfield",
-          template: "<div></div>",
-          methods: {
-            validate: mockValidate,
-          },
-        },
-        "real-digital-button": RealDigitalButton,
-      },
-    });
-    const button = wrapper.findComponent(RealDigitalButton);
-    await button.trigger("click");
+    const wrapper = getWrapper(mockValidate);
+    await submit(wrapper);
     expect(mockValidate.mock.calls.length).toBe(1);
+  });
+
+  it("dose nothing when textfields are invalid", async () => {
+    const mockValidate = jest.fn();
+    mockValidate.mockImplementation(() => false);
+    const wrapper = getWrapper(mockValidate);
+    await submit(wrapper);
+    expect(wrapper.emitted("onResponse")).toBeFalsy();
+  });
+
+  it("emits onSubmit when validation is successful", async () => {
+    const mockValidate = jest.fn();
+    mockValidate.mockImplementation(() => true);
+    const wrapper = getWrapper(mockValidate);
+    await submit(wrapper);
+    expect(wrapper.emitted("onSubmit")[0][0]).toMatchObject({
+      test: 2,
+    });
+  });
+  it("emits onResponse with response body as payload when validation is successful", async () => {
+    axios.mockImplementation(() => ({
+      json: {
+        test: 2,
+      },
+    }));
+    const mockValidate = jest.fn();
+    mockValidate.mockImplementation(() => true);
+    const wrapper = getWrapper(mockValidate);
+    await submit(wrapper);
+    expect(wrapper.emitted("onResponse")[0][0]).toMatchObject({
+      test: 2,
+    });
   });
 });
