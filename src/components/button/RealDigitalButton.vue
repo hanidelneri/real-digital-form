34aafe2e1b2def3772fd5b529358f2a6ff5e219f
Hanie Gharedaghi <h7.gharedaghi@gmail.com>
2021-10-24 11:27:18 +0330

    adding loading state to button

--- src/components/button/RealDigitalButton.vue
+++ src/components/button/RealDigitalButton.vue
@@ -1,6 +1,7 @@
 <template>
-  <button :disabled="disabled" @click="onClick">
-    <slot />
+  <button class="button" :disabled="loading" @click="onClick">
+    <span v-if="loading">Loading...</span>
+    <slot v-else />
   </button>
 </template>
 <script>
@@ -9,18 +10,41 @@ export default {
   name: REAL_DIGITAL_BUTTON,
   data() {
     return {
-      disabled: false,
+      loading: false,
     };
   },
   methods: {
     async onClick() {
       if (this.$parent && this.$parent.submit) {
-        this.disabled = true;
+        this.loading = true;
         await this.$parent.submit();
-        this.disabled = false;
+        this.loading = false;
       }
       this.$emit("click");
     },
   },
 };
 </script>
+<style lang="sass" scoped>
+.button
+  color: #222020
+  text-transform: uppercase
+  font-size: 14px
+  letter-spacing: 1px
+  height: 38px
+  width: 100%
+  line-height: 38px
+  overflow: hidden
+  background: #4dbecf
+  border-radius: 3px
+  box-shadow: 0 15px 30px rgba(black,.1)
+  border: 0
+  cursor: pointer
+  transition: all .3s ease
+  &:hover,
+  &:focus
+    box-shadow: 0 5px 15px rgba(black,.1)
+  &:disabled
+    opacity: 0.3
+    pointer-events: none
+</style>
