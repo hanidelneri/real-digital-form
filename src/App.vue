d6aa1b087be4d1e45cc8af210a257e97fd1edb3f
Hanie Gharedaghi <h7.gharedaghi@gmail.com>
2021-10-24 11:47:59 +0330

    removing subject validation

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
       <real-digital-form action="https://httpbin.org/post" method="POST">
         <real-digital-textfield name="name" validation="[a-z]+" />
         <real-digital-textfield name="phone" validation="[0-9]+" />
-        <real-digital-textfield name="subject" validation="[0-9]+" />
+        <real-digital-textfield name="subject" />
         <real-digital-button> Submit </real-digital-button>
       </real-digital-form>
     </div>
